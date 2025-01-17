a530b5934a664a70a537e05b232e77166f6902ef
shahedul-alam <shaheedalamcontact@gmail.com>
2025-01-17 12:53:00 +0600

    footer section added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
   return (
     <div className="font-syne">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
